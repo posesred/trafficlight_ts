5eb26ec039abb4dbcb3f0f37a4d2d1301d736a33
Sanatjon Burkhanov <sburhanov1977@gmail.com>
2024-02-20 17:19:11 -0500

    trafficlight state

--- src/ClassTrafficLight.tsx
+++ src/ClassTrafficLight.tsx
@@ -1,17 +1,42 @@
 import { Component } from "react";
-
+interface State {
+  trafficColor: number;
+}
 export class ClassTrafficLight extends Component {
+  state: State = {
+    trafficColor: 0,
+  };
+  changeColor = () => {
+    this.setState((current: State) => ({
+      trafficColor: (current.trafficColor + 1) % 3,
+    }));
+  };
   render() {
     return (
       <div className="traffic-light-box">
         <h2>Class Traffic Light</h2>
         <div className="traffic-light">
           {/* Background color can be black | yellow | red | green */}
-          <div className="circle red"></div>
-          <div className="circle black"></div>
-          <div className="circle black"></div>
+          {/* To figure out that interfrace error took 20minutes and now there is eslintrc error */}
+          <div
+            className={`circle ${
+              this.state.trafficColor === 0 ? "red" : "black"
+            }`}
+          ></div>
+          <div
+            className={`circle ${
+              this.state.trafficColor === 2 ? "yellow" : "black"
+            }`}
+          ></div>
+          <div
+            className={`circle ${
+              this.state.trafficColor === 1 ? "green" : "black"
+            }`}
+          ></div>
         </div>
-        <button className="next-state-button">Next State</button>
+        <button className="next-state-button" onClick={this.changeColor}>
+          Next State
+        </button>
       </div>
     );
   }
--- src/FunctionalTrafficLight.tsx
+++ src/FunctionalTrafficLight.tsx
@@ -1,17 +1,23 @@
+import { useState } from "react";
 export const FunctionalTrafficLight = () => {
+  const [color, setColor] = useState<number>(0);
+  const changeColor = (): void => {
+    setColor((current: number) => (current + 1) % 3);
+  };
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
         {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        {/* Can I pass Any type on everything then no problem you know */}
+        {/* I didnt know which props to pass so I tried some googling */}
+        <div className={`circle ${color === 0 ? "red" : "black"}`}></div>
+        <div className={`circle ${color === 2 ? "yellow" : "black"}`}></div>
+        <div className={`circle ${color === 1 ? "green" : "black"}`}></div>
       </div>
-      <button className="next-state-button">Next State</button>
+      <button className="next-state-button" onClick={changeColor}>
+        Next State
+      </button>
     </div>
   );
 };
-
-
-
